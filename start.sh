26bc499d4640924cb8c88079bb52d68364da0af6
nnlgsakib <nlgarts@outlook.com>
2024-07-14 16:57:41 +0600

    updated sc

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-biz --testnet --datadir nodetest --ipcdisable --mine --allow-insecure-unlock=true --unlock  0xd859B09a9b5b2e717F8049a01612D6e2a0587549  --password ps --http --miner.gasprice 1000000000000
+biz --testnet --datadir nodetest --ipcdisable --mine --allow-insecure-unlock=true --unlock  0xd859B09a9b5b2e717F8049a01612D6e2a0587549  --password ps --http --miner.gasprice 1000000000000 --rpc.allow-unprotected-txs
